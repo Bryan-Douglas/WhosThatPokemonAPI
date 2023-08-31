98fec8809210cb97286116e3891a048323a92fb7
Bryan-Douglas <brydougwork@gmail.com>
2023-08-30 23:15:56 -0700

    added some of the client logic over to the server since it made more sense to have it server side.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require('cors'); // Cross Origin Resource Sharing
 require('dotenv').config(); // Configures environment variables
 const app = express(); // Calls the app to use express
 const { PORT, BACKEND_URL, CORS_ORIGIN } = process.env; // These are the environment variables to be used
-const routes = require('./routes/routes');
 const axios = require('axios'); // Imports axios which is used for the API call
 
 /*
@@ -12,35 +11,18 @@ const axios = require('axios'); // Imports axios which is used for the API call
 * express.json() allows access to incoming data posted to the server as part of the req.body
 */
 
-app.use(cors({ origin: CORS_ORIGIN })); // Makes the app use the cors origin
+const corsOptions = {
+    origin: 'http://localhost:3000', // Replace with your React app's URL
+    methods: 'GET,POST', // Add other methods if needed
+  };
+  
+  app.use(cors(corsOptions));
+
+// app.use(cors({ origin: CORS_ORIGIN })); // Makes the app use the cors origin
 app.use(express.json()); // 
 app.use(express.static('public')); //
-
-app.get('/api/getRandomPokemon', async (req, res) => {
-    try {
-        const randomPokemonId = generateRandomPokemonId();
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`);
-        res.json(response.data);
-    }
-    catch (error) {
-        console.error('Error retrieving Pokemon data', error);
-        res.status(500).json(`Server is running on port ${PORT}`);
-    }})
-
-function generateRandomPokemonId() {
-    return Math.floor(Math.random() * 1004) + 1;
-}    
-
-app.use('/pokemon', routes);
-
-app.get('/pokemon', (req, res) => {
-    res.send(`'Express is running'`);
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-})
+const routes = require('./routes/routes');
+app.use('/api', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
--- routes/routes.js
+++ routes/routes.js
@@ -2,30 +2,79 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-async function getAllPokemonData(url, allPokemonData = []) {
+const generateRandomPokemonId = () => {
+    return Math.floor(Math.random() * 1004) + 1;
+  };
+
+function decimetersToFeetAndInches(decimeters) {
+    const feet = decimeters * 0.328084;
+    const remainingFeet = Math.round(feet);
+    const totalInches = feet * 12;
+    const inches = Math.floor(totalInches);
+    const remainingInches = Math.round((totalInches - inches));
+
+    return { feet: remainingFeet, inches: remainingInches };
+}
+
+function hectogramsToPounds(hectograms) {
+    const pounds = hectograms * 0.220462;
+    const remainingPounds = Math.round(pounds);
+
+    return { pounds: remainingPounds };
+}
+
+router.get('/getRandomPokemon', async (req, res) => {
     try {
-        const response = await axios.get(url);
-        const { results, next } = response.data;
-
-        allPokemonData.push(...results);
-
-        if (next) {
-            return await getAllPokemonData(next, allPokemonData);
-        } else {
-            return allPokemonData;
-        }
+        const randomPokemonId = generateRandomPokemonId();
+        const pokemonData = await fetchPokemonData(randomPokemonId);
+        res.json(pokemonData);
     } catch (error) {
-        console.error(error);
-        throw new Error('Error fetching data from PokeApi');
+        console.error('Error retrieving active pokemon data', error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+});
+
+async function fetchPokemonData(pokemonId) {
+    try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+        const altResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`);
+        const spriteUrl = response.data.sprites.other['official-artwork'].front_default;
+        const name = response.data.name;
+        const types = response.data.types.map(typeObj => typeObj.type.name);
+        const height = decimetersToFeetAndInches(response.data.height);
+        const weight = hectogramsToPounds(response.data.weight);
+        const genera = altResponse.data.genera[7].genus;
+        const flavorText = altResponse.data.flavor_text_entries[0].flavor_text;
+        const cleanFlavorText = flavorText.replace(//, ' ');
+        const processedData = {
+            id: pokemonId,
+            spriteUrl: spriteUrl,
+            name: name,
+            types: types,
+            height: height,
+            weight: weight,
+            genera: genera,
+            flavor_text_entries: cleanFlavorText
+        };
+        return processedData;
+    } catch (error) {
+        console.error('Error fetching pokemon data', error);
+        throw error;
     }
 }
 
-router.get('/', async (req, res) => {
+router.post('/handleCorrectGuess', (req, res) => {
     try {
-        const allPokemon = await getAllPokemonData('https://pokeapi.co/api/v2/pokemon/');
-        res.json(allPokemon);
+        const { pokemon } = req.body;
+        const handleCorrectGuess = (pokemon) => {
+            setCorrectGuesses((prevGuesses) => [...prevGuesses, pokemon]);
+            setScore((prevScore) => prevScore + 1);
+            handleCorrectGuess(activePokemon);
+        };
+        res.json({ score: setScore });
     } catch (error) {
-        res.status(500).json({ error: 'Error fetching data from PokeAPI' });
+        console.error('Error handling correct guess', error);
+        res.status(500).json({ error: 'An error occurred' });
     }
 });
 
